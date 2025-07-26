b797199998ef6855855f7b8cafb4015ba0f1b72c
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-07-26 13:20:14 -0300

    feat: adiciona repositórios

---
+++ src/main/java/com/oak/finance_manager/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.oak.finance_manager.repositories;
+
+import com.oak.finance_manager.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
---
+++ src/main/java/com/oak/finance_manager/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.oak.finance_manager.repositories;
+
+import com.oak.finance_manager.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
